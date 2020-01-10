359fa0d4bb5d2aa910deff5052cd96c7286289fe
David Cantoran <davidcantoran36@gmail.com>
2020-01-10 14:46:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Grizzie", 5, "Female", "Brown", "Aggressive", );
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Grizzie", 5, "Female", "Brown", "Aggressive", 1);
