8b4ba51ef04c2e0130141c9fea0a8df4fb2e3a38
David Cantoran <davidcantoran36@gmail.com>
2020-01-10 14:58:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Grizzie", 5, "Male", "Brown", "Nice", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Charmin", 3, "Male", "Mix", "Gentle", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Cassie", 6, "Female", "Spotted", "Gentle", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Fedor", 2, "Male", "Brown", "Calm", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Lizzie", 5, "Female", "Brown", "Hyper", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Grizzie", 5, "Male", "Brown", "Nice", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Charmin", 3, "Male", "Mix", "Gentle", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Cassie", 6, "Female", "Spotted", "Gentle", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Fedor", 2, "Male", "Brown", "Calm", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Lizzie", 5, "Female", "Brown", "Hyper", 1);
 
