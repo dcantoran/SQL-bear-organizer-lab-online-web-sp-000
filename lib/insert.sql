965dddaf4335a962cb69877a09fa385ecc3947c2
David Cantoran <davidcantoran36@gmail.com>
2020-01-10 15:03:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Fe
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Lizzie", 5, "Female", "Brown", "Active", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Dopey", 7, "Male", "Polar", "Funny", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Lola", 14, "Female", "Black", "Active", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("", 5, "Female", "Brown", "Hyper", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "Female", "Brown", "Hyper", 1);
