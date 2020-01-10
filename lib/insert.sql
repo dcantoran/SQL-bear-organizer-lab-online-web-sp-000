9628fb8c4525de9c66c80aa1eac0b4355c97eeea
David Cantoran <davidcantoran36@gmail.com>
2020-01-10 15:20:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grizzie", 5, "Male", "Brown", "Nice", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Charmin", 3, "Male", "Mix", "Gentle", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Cassie", 6, "Female", "Spotted", "Gentle", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Fedor", 2, "Male", "Brown", "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Lizzie", 5, "Female", "Brown", "Active", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Dopey", 7, "Male", "Polar", "Funny", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Lola", 14, "Female", "Black", "Active", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "Male", "Brown", "Aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grizzie", 5, "M", "Brown", "Nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Charmin", 3, "M", "Mix", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Cassie", 6, "F", "Spotted", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Fedor", 2, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Lizzie", 5, "F", "Brown", "Active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Dopey", 7, "M", "Polar", "Funny", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Lola", 14, "F", "Black", "Active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "M", "Brown", "Aggressive", 0);
