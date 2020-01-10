331ba5ca2221e45a60e70e2295ca58b54b872472
David Cantoran <davidcantoran36@gmail.com>
2020-01-10 15:05:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Grizzie", 5, "Male", "Brown", "Nice", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Charmin", 3, "Male", "Mix", "Gentle", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Cassie", 6, "Female", "Spotted", "Gentle", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Fedor", 2, "Male", "Brown", "Calm", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Lizzie", 5, "Female", "Brown", "Active", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Dopey", 7, "Male", "Polar", "Funny", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Lola", 14, "Female", "Black", "Active", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "Male", "Brown", "Aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grizzie", 5, "Male", "Brown", "Nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Charmin", 3, "Male", "Mix", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Cassie", 6, "Female", "Spotted", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Fedor", 2, "Male", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Lizzie", 5, "Female", "Brown", "Active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Dopey", 7, "Male", "Polar", "Funny", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Lola", 14, "Female", "Black", "Active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "Male", "Brown", "Aggressive", 0);
